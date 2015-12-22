e8ea0e16eca3c47b504e0f925319fb5935cf6f18
mauricioaschmitz <mauricioantonioli@gmail.com>
2015-12-22 13:33:32 -0200

    Atualizado CodeIgniter para versão 3.0.3 e BootStrap para versão 3.3.6.

--- application/views/usuario_consulta.php
+++ application/views/usuario_consulta.php
@@ -1,20 +1,19 @@
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title><?= $this->lang->line('page_title_consult_user'); ?></title>
-	<script src="includes/bootstrap/js/jquery.min"></script>
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="<?= base_url('includes/js/jquery-2.1.1.js') ?>"></script><!-- import jquery -->
+	<script src="<?= base_url('includes/bootstrap/js/bootstrap.min.js') ?>"></script>
+	<script src="<?= base_url('includes/js/sorttable.js') ?>"></script><!-- import ordenação colunas tabela -->
+	<script src="<?= base_url('includes/js/funcoesjs.js') ?>"></script><!-- import funções js -->
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="<?= base_url('includes/bootstrap/css/bootstrap.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('includes/bootstrap/css/bootstrap-responsive.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('includes/css/abas.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('includes/css/estilo.css') ?>">
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="<?= base_url('includes/bootstrap/js/bootstrap.min.js') ?>"></script>	
-	<script src="<?= base_url('includes/js/jquery-2.1.1.js') ?>"></script><!-- import jquery -->
-	<script src="<?= base_url('includes/js/sorttable.js') ?>"></script><!-- import ordenação colunas tabela -->
-	<script src="<?= base_url('includes/js/funcoesjs.js') ?>"></script><!-- import funções js -->
 </head>
 <body>
 	<div class="container-fluid">
