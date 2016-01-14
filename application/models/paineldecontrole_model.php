ac362d59f36e4d29f89e006a72da97181243e5b1
mauricioaschmitz <mauricioantonioli@gmail.com>
2016-01-13 22:57:19 -0200

    Revisado código SQL get_all_usosala

--- application/models/paineldecontrole_model.php
+++ application/models/paineldecontrole_model.php
@@ -1,64 +1,79 @@
 <?php
-/**
-	* 2015
-	* Desenvolvido por: Mateus Perego
-	* Email: mateusperego@yahoo.com.br
-	* Projeto de conclusão de curso
-	* UPF - Ciência da Computação
-	*/	
 
-class paineldecontrole_model extends CI_Model{
-	//pega todos os registros das salas em uso
-	function get_all_usosala(){
-		//$this->db->select("DATE_FORMAT(horaInicio,('%H:%i:%S')) as hora, salacirurgia.desc, codUsoSala");
-		$this->db->select("horaInicio as hora,salacirurgia.desc, codUsoSala");
-		$this->db->from ('usosala,salacirurgia');
-		$this->db->where('usosala.codUsoSala = salacirurgia.codSala');
-		$this->db->where('ativa = 1');
-		$this->db->order_by('codUsoSala');
-		$query = $this->db->get();
+class paineldecontrole_model extends CI_Model {
 
-		return $query->result();
-	}
+    //pega todos os registros das salas em uso
+    function get_all_usosala() {
+        $this->db->select('usosala.codUsoSala, salacirurgia.desc, usosala.horaInicio, usosala.horaFinal');
+        $this->db->from('usosala, salacirurgia');
+        $this->db->where('usosala.codUsoSala = salacirurgia.codSala');
+        $this->db->where('usosala.ativa = 1');
+        $this->db->order_by('usosala.codUsoSala');
+        $query = $this->db->get();
 
-	// get todos codusosala
-	function get_all_codusosala(){
-		$this->db->select("codUsoSala");
-		$this->db->from ('usosala');
-		$this->db->where('ativa = 1');
-		$this->db->order_by('codUsoSala');
-		$query = $this->db->get();
+        return $query->result();
+    }
 
-		return $query->result();
-	}
-	
-	//get todos os dados do codUsoSala
-	function get_by_id_usosala($codUsoSala){
-		$this->db->select("horaInicio as hora, salacirurgia.desc, codUsoSala");
-		$this->db->from ('usosala,salacirurgia');
-		$this->db->where('usosala.codUsoSala = salacirurgia.codSala');
-		$this->db->where('ativa = 1');
-		$this->db->where('codUsoSala',$codUsoSala);
-		$query=$this->db->get();
+    /*function get_all_usosala(){
+      //$this->db->select("DATE_FORMAT(horaInicio,('%H:%i:%S')) as hora, salacirurgia.desc, codUsoSala");
+      $this->db->select("horaInicio as hora,salacirurgia.desc, codUsoSala");
+      $this->db->from ('usosala,salacirurgia');
+      $this->db->where('usosala.codUsoSala = salacirurgia.codSala');
+      $this->db->where('ativa = 1');
+      $this->db->order_by('codUsoSala');
+      $query = $this->db->get();
 
-		return $query->row(0);
-	}
+      return $query->result();
+      }*/
 
-	//get all alertas
-	function get_all_alertas(){
-		$this->db->select("uso.codUsoSala");
-		$this->db->from ('capturaatual cap, usosalacaptura uso, usosala');
-		$this->db->where('cap.codCaptura NOT IN (SELECT a.codCaptura FROM alerta a)');
-		$this->db->where('uso.codUsoSala = uso.codUsoSala');
-		$this->db->where('uso.codCaptura = cap.codCaptura');
-		$this->db->where('usosala.codUsoSala = uso.codUsoSala');
-		$this->db->where('usosala.ativa = 1');
-		$this->db->where('codEvento = 1');
-		$query = $this->db->get();
+    // get todos codusosala
+    function get_all_codusosala() {
+        $this->db->select('*');
+        $this->db->from('usosala');
+        //$this->db->where('ativa = 1');
+        //$this->db->order_by('codUsoSala');
+        $query = $this->db->get();
 
-		return $query->result();
-	}
+        return $query->result();
+    }
 
-} 
+    //get todos os dados do codUsoSala
+    function get_by_id_usosala($codUsoSala) {
+        $this->db->select("horaInicio as hora, salacirurgia.desc, codUsoSala");
+        $this->db->from('usosala,salacirurgia');
+        $this->db->where('usosala.codUsoSala = salacirurgia.codSala');
+        $this->db->where('ativa = 1');
+        $this->db->where('codUsoSala', $codUsoSala);
+        $query = $this->db->get();
 
-?>
+        return $query->row(0);
+    }
+
+    //get all alertas
+    function get_all_alertas() {
+        $this->db->select('usoCap.codUsoSala');
+        $this->db->from('capturaatual cap, usosalacaptura usoCap, usosala');
+        $this->db->where('usoCap.codCaptura = cap.codCaptura');
+        $this->db->where('usosala.codUsoSala = usoCap.codUsoSala');
+        $this->db->where('usosala.ativa = 1');
+        $query = $this->db->get();
+        
+        return $query->result();
+    }
+    
+    
+    /**function get_all_alertas() {
+        $this->db->select("uso.codUsoSala");
+        $this->db->from('capturaatual cap, usosalacaptura uso, usosala');
+        $this->db->where('cap.codCaptura NOT IN (SELECT a.codCaptura FROM alerta a)');
+        $this->db->where('uso.codUsoSala = uso.codUsoSala');
+        $this->db->where('uso.codCaptura = cap.codCaptura');
+        $this->db->where('usosala.codUsoSala = uso.codUsoSala');
+        $this->db->where('usosala.ativa = 1');
+        $this->db->where('codEvento = 1');
+        $query = $this->db->get();
+
+        return $query->result();
+    }*/
+
+}
