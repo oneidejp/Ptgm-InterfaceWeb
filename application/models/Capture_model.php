24e9df8688aca4a774323fbdf5f53238c42927aa
mauricioaschmitz <mauricioantonioli@gmail.com>
2016-07-14 21:03:56 -0300

    Reorganização do projeto utilizando template, padronização de tags php, correção de bugs diversos e melhorias no visual.

---
+++ application/models/Capture_model.php
@@ -0,0 +1,11 @@
+<?php
+
+        /**
+	* 2016
+	* Desenvolvido por: Maurício Antonioli Schmitz
+	* Projeto de Mestrado em Computação Aplicada - PPGCA/UPF
+	*/
+
+class nome_arquivo_php_sem_extensao extends CI_Model{
+    
+}
