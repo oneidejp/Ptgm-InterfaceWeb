a541600e3d5c05d1265c626e27ebe9eb89dbc1c0
mauricioaschmitz <mauricioantonioli@gmail.com>
2016-07-29 17:00:09 -0300

    Fix problems with charts.

--- application/views/V_home_view.php
+++ application/views/V_home_view.php
@@ -1,9 +1,8 @@
 <div class="container-fluid">
     <div class="row-fluid">
-        <div class="col-md-1 col-xs-1" id="esquerda"></div>
-        <div class="col-md-10 col-xs-10" id="centro">
+        <div class="col-md-offset-1 col-md-10 col-xs-10" id="centro">
             <div id="aba">
-                <h1 class="text-center">
+                <h1 class="text-center" style="margin-top: 100px;">
                     <?php
                     echo $this->lang->line('page_title_v_home') . " ";
                     echo $this->session->userdata('nome') . " ";
