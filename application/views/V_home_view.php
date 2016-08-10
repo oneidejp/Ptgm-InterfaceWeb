79da6c1c72f53f21f7b4dd78729ec7193d37ca3c
mauricioaschmitz <mauricioantonioli@gmail.com>
2016-08-10 03:29:04 -0300

    CSS to create border on "Div"

--- application/views/V_home_view.php
+++ application/views/V_home_view.php
@@ -1,16 +1,13 @@
 <div class="container-fluid">
     <div class="row-fluid">
-        <div class="col-md-offset-1 col-md-10 col-xs-10" id="centro">
-            <div id="aba">
-                <h1 class="text-center" style="margin-top: 100px;">
-                    <?php
-                    echo $this->lang->line('page_title_v_home') . " ";
-                    echo $this->session->userdata('nome') . " ";
-                    echo $this->lang->line('logged');
-                    ?>
-                </h1>
-            </div>
+        <div class="col-md-offset-1 col-md-10 col-xs-10" id="borda">
+            <h1 class="text-center" style="margin-top: 100px; margin-bottom: 100px;">
+                <?php
+                echo $this->lang->line('page_title_v_home') . " ";
+                echo $this->session->userdata('nome') . " ";
+                echo $this->lang->line('logged');
+                ?>
+            </h1>
         </div>
-        <div class="col-md-1 col-xs-1" id="direita"></div>
     </div>
 </div>
